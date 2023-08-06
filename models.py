91b2f7899bb6f24b506bd8223f55cbf9543e9b61
Oliver Holmes <oholmes@anditi.com>
2023-08-07 01:43:00 +0930

    Remove old User SQL Model

--- models.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-from sqlmodel import Field, SQLModel
-
-
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
